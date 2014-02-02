f7dbb9c3182d07e325497716a744ffcb87313297
Thorsten Tarrach <thorstent@live.com>
2014-02-02 12:34:14 +0100

    added new version of z3 and cbmc, added 32 bin version of files

--- make-z3.sh
+++ make-z3.sh
@@ -8,4 +8,4 @@ SCRIPTPATH=`dirname "$SCRIPT"`
 cd $SCRIPTPATH
 
 jar cf z3.jar smt-solvers
-mvn deploy:deploy-file -Durl=file://$SCRIPTPATH/repo/ -Dfile=z3.jar -DgroupId=smt-solvers -DartifactId=z3 -Dpackaging=jar -Dversion=4.3.1
+mvn deploy:deploy-file -Durl=file://$SCRIPTPATH/repo/ -Dfile=z3.jar -DgroupId=smt-solvers -DartifactId=z3 -Dpackaging=jar -Dversion=4.3.2
